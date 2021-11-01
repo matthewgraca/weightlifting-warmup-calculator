49e303f033ed2d5f85265a9f7716cbbc267c5763
matthewgraca <mgraca@cpp.edu>
2021-10-31 23:16:48 -0700

    added tests for sets in the constructor

--- app/src/test/java/com/example/weightliftingwarmupcalculator/SetSchemeTest.kt
+++ app/src/test/java/com/example/weightliftingwarmupcalculator/SetSchemeTest.kt
@@ -192,12 +192,51 @@ class SetSchemeTest{
     }
 
     // test multiple sets
-    //@Test
-    fun test10SetsPounds1(){
+    @Test
+    fun testMultipleSetsPounds1(){
         val scheme = SetScheme(10, 175, Pound())
         val setScheme = scheme.getScheme()
-        val solution = intArrayOf(60, 75, 90, 105, 120, 135)
-        val errorMsg = "Expected " + solution.contentToString() + ", Actual " + setScheme.contentToString()
-        assertArrayEquals(errorMsg, solution, setScheme)
+        val solution = intArrayOf(60, 70, 85, 95, 110, 125, 135, 150, 160, 175)
+        assertArrayEquals(solution, setScheme)
+    }
+
+    @Test
+    fun testMultipleSetsPounds2(){
+        val scheme = SetScheme(7, 335, Pound())
+        val setScheme = scheme.getScheme()
+        val solution = intArrayOf(85, 130, 170, 210, 250, 295, 335)
+        assertArrayEquals(solution, setScheme)
+    }
+
+    @Test
+    fun testMultipleSetsPounds3(){
+        val scheme = SetScheme(1, 300, Pound())
+        val setScheme = scheme.getScheme()
+        val solution = intArrayOf(300)
+        assertArrayEquals(solution, setScheme)
+    }
+
+    @Test
+    fun testMultipleSetsKilos1(){
+        val scheme = SetScheme(10, 270, Kilogram())
+        val setScheme = scheme.getScheme()
+        val solution = intArrayOf(45, 70, 95, 120, 145, 170, 195, 220, 245, 270)
+        assertArrayEquals(solution, setScheme)
+    }
+
+    @Test
+    fun testMultipleSetsKilos2(){
+        val scheme = SetScheme(7, 332, Kilogram())
+        val setScheme = scheme.getScheme()
+        val solution = intArrayOf(65, 110, 155, 200, 245, 285, 332)
+        assertArrayEquals(solution, setScheme)
+    }
+
+    @Test
+    fun testMultipleSetsKilos3(){
+        val scheme = SetScheme(1, 123, Kilogram())
+        val setScheme = scheme.getScheme()
+        val solution = intArrayOf(123)
+        assertArrayEquals(solution, setScheme)
     }
 }
